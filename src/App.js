d711d6945404f530b10b3db867bf8cc86237bd80
Aurélien Gendrey <aurelien@MacBook-Air-de-Aurelien.local>
2023-12-21 15:56:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  // ... Autres fonctions et logique ici
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mes tâches</h1>
+      <TaskForm addTask/>
+      <TaskList tasks={tasks} />
     </div>
   );
 }
